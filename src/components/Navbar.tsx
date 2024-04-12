643651260578d266a8ec822118c1c1837bef90dd
z4heed <szasaa10@gmail.com>
2024-04-13 03:10:12 +0530

    "Update Instructors component and add missing import in Navbar component"
    This commit updates the Instructors component to change the instructor details and related text to reflect developers instead of music instructors. Additionally, it adds a missing import statement

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
-import Link from "next/link";
+import Link from "next/link";  // "next/link" add this here 
 
 function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
