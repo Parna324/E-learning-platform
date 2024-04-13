1b561ec59f21223cbd797eeeb3b607250f2501f8
smartcraze <smartcraze>
2024-04-13 13:44:20 +0530

    yes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,16 @@ import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
-// import { TextReveal } from "@/components/Textreveal";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
+
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      
       <FeaturedCourses />
       <WhyChooseUs />
       <MusicSchoolTestimonials />
--- src/conf/config.ts
+++
@@ -1,6 +0,0 @@
-const conf = {
-    appwriteUrl: String(process.env.NEXT_PUBLIC_APPWRITE_URL),
-    appwriteProjectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
-}
-
-export default conf
