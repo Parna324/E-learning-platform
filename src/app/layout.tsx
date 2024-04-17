fc2a1217e74450cf089765df5159fd5d0871baca
smartcraze <smartcraze>
2024-04-17 15:43:42 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Build-a-Thon",
-  description: "E-Learning Platform",
+const metadata: Metadata = {
+  title: "Smartcraze by Suraj Vishwakarma: Full Stack Developer and Code Enthusiast",
+  description: "Welcome to Smartcraze by Suraj Vishwakarma! Explore the world of a passionate full stack developer, where code meets creativity. Get insights into development projects, tutorials, and technology trends.",
 };
 
 export default function RootLayout({
@@ -17,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <Head>
+        <link rel="shortcut icon" href="smartcraze.png" />
+      </Head>
       <body className={inter.className}>
         <div className="relative w-full flex items-center justify-center ">
           <Navbar />
